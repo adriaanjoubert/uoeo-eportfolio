24424b027fc2ea41656c5257bc6536cada8c9e23
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2024-05-07 18:19:22 +0100

    Add learning outcomes

--- src/pages/launching_into_computer_science.rs
+++ src/pages/launching_into_computer_science.rs
@@ -7,7 +7,21 @@ pub fn launching_into_computer_science_page() -> Html {
     html! {
         <>
             <Nav />
-            <p>{ "TODO" }</p>
+            <h1>{ "Learning outcomes" }</h1>
+            <ul>
+                <li>
+                    { "Identify and explain the architecture, structure and functionality of basic components of computer system." }
+                </li>
+                <li>
+                    { "Demonstrate a critical understanding of core data structures and programming concepts, including algorithm computability." }
+                </li>
+                <li>
+                    { "Critically evaluate the functionality of different types of software, i.e., operating system, utility programs, languages and applications." }
+                </li>
+                <li>
+                    { "Critically appraise the emerging trends in the field, such as cloud computing, big data, cyber security, and the professional and ethical requirements for dealing with such contemporary computer-based technologies. "}
+                </li>
+            </ul>
         </>
     }
 }
