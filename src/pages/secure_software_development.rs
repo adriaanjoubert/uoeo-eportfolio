7cc68623b0e552868b714925bc8014c73873d779
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2024-08-03 20:42:08 +0300

    Add learning outcomes

--- src/pages/secure_software_development.rs
+++ src/pages/secure_software_development.rs
@@ -7,7 +7,21 @@ pub fn secure_software_development_page() -> Html {
     html! {
         <>
             <Nav />
-            <p>{ "TODO" }</p>
+            <h1>{ "Learning outcomes" }</h1>
+            <ul>
+                <li>
+                    { "Identify and manage security risks as part of a software development project." }
+                </li>
+                <li>
+                    { "Critically analyse development problems and determine appropriate methodologies, tools and techniques (including program design and development) to solve them." }
+                </li>
+                <li>
+                    { "Design and develop/adapt computer programs and to produce a solution that meets the design brief and critically evaluate solutions that are produced." }
+                </li>
+                <li>
+                    { "Systematically develop and implement the skills required to be effective member of a development team in a virtual professional environment, adopting real-life perspectives on team roles and organisation."}
+                </li>
+            </ul>
         </>
     }
 }
