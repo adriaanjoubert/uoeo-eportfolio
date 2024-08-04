5078fc9c92fd6a916d42608bbf12cb78aa143a3a
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2024-08-04 21:26:13 +0300

    Add artefact

--- src/pages/secure_software_development.rs
+++ src/pages/secure_software_development.rs
@@ -22,6 +22,14 @@ pub fn secure_software_development_page() -> Html {
                     { "Systematically develop and implement the skills required to be effective member of a development team in a virtual professional environment, adopting real-life perspectives on team roles and organisation."}
                 </li>
             </ul>
+            <h1>{ "Artefacts" }</h1>
+            <ul>
+                <li>
+                    { "Unit 1: A UML sequence diagram was created illustrating the security vulnerability of failing to enforce record ownership. An example was given and a solution was proposed. "}
+                    <a href="https://www.my-course.co.uk/mod/forum/discuss.php?d=245616">{"Link to forum post."}</a>
+                    { " Learning outcome 1: Identify & manage security risks as part of a software development project." }
+                </li>
+            </ul>
         </>
     }
 }
