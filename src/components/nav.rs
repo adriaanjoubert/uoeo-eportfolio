90e930aa50185ec3d322ab4c4d822c8dbe0aa399
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2024-10-21 18:36:51 +0300

    Update nav

--- src/components/nav.rs
+++ src/components/nav.rs
@@ -6,27 +6,36 @@ use crate::router::Route;
 #[function_component(Nav)]
 pub fn nav() -> Html {
     html! {
-        <ul>
-            <li>
-                <Link<Route> to={Route::Home}>
-                    { "Home" }
-                </Link<Route>>
-            </li>
-            <li>
-                <Link<Route> to={Route::LaunchingIntoComputerScience}>
-                    { "Launching Into Computer Science" }
-                </Link<Route>>
-            </li>
-            <li>
-                <Link<Route> to={Route::SecureSoftwareDevelopment}>
-                    { "Secure Software Development" }
-                </Link<Route>>
-            </li>
-            <li>
-                <Link<Route> to={Route::ObjectOrientedProgramming}>
-                    { "Object-oriented Programming" }
-                </Link<Route>>
-            </li>
-        </ul>
+        <nav class="layout-navbar shadow-none py-0">
+            <div class="container">
+                <div class="navbar navbar-expand-lg landing-navbar px-3 px-md-8">
+                    <div class="navbar-brand app-brand demo d-flex py-0 me-4 me-xl-8">
+                        <button class="navbar-toggler border-0 px-0 me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="tf-icons bx bx-menu bx-lg align-middle text-heading fw-medium"></i>
+                        </button>
+                        <Link<Route> classes={classes!("app-brand-link")} to={Route::Home}>
+                            <span class="app-brand-text demo menu-text fw-bold ms-2 ps-1">{ "Home" }</span>
+                        </Link<Route>>
+                    </div>
+
+                    <div class="collapse navbar-collapse landing-nav-menu" id="navbarSupportedContent">
+                        <button class="navbar-toggler border-0 text-heading position-absolute end-0 top-0 scaleX-n1-rtl p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="tf-icons bx bx-x bx-lg"></i>
+                        </button>
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <Link<Route> classes={classes!("nav-link", "fw-medium")} to={Route::LaunchingIntoComputerScience}>{ "Launching Into Computer Science" }</Link<Route>>
+                            </li>
+                            <li class="nav-item">
+                                <Link<Route> classes={classes!("nav-link", "fw-medium")} to={Route::SecureSoftwareDevelopment}>{ "Secure Software Development" }</Link<Route>>
+                            </li>
+                            <li class="nav-item">
+                                <Link<Route> classes={classes!("nav-link", "fw-medium")} to={Route::ObjectOrientedProgramming}>{ "Object-oriented Programming" }</Link<Route>>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     }
 }
